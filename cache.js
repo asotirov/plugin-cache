c185b417ce930e8cfa39653d997b22d99431efd0
Anton Sotirov <anton.k.sotirov@gmail.com>
2018-08-06 14:55:20 +0300

    Cache refactored to use redis instead of fs caching

--- cache.js
+++ cache.js
@@ -1,19 +1,30 @@
 'use strict';
 const cacheManager = require("cache-manager"),
-    fsStore = require('cache-manager-fs');
+    redisStore = require('cache-manager-redis');
 
 module.exports = function (options, imports, register) {
-    const cache = cacheManager.caching({
-        store: fsStore,
-        options: {
-            ttl: 0,
-            maxsize: 1000 * 1000 * 1000 /* max size in bytes on disk */,
-            path: 'diskcache',
-            preventfill: true,
-            zip: true
-        }
-    });
-    register(null, {
-        cache
-    });
+    try {
+        const cache = cacheManager.caching({
+            ignoreCacheErrors: true,
+            store: redisStore,
+            host: options.redis.host,
+            port: options.redis.port,
+            options: {
+                ttl: Number.MAX_VALUE, //CACHE ME OUTSIDE, HOW 'BOUT DAT
+                maxsize: 1000 * 1000 * 1000 /* max size in bytes on disk */
+            },
+            retry_strategy: function (options) {
+                if (options.attempt > 1) {
+                    // Stop retrying after first attempts.
+                    return undefined;
+                }
+                // Increase reconnect delay by 150ms.
+                return options.attempt * 150;
+            }
+        });
+
+        register(null, {cache});
+    } catch (err) {
+        register(err);
+    }
 };
