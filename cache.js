63acd1fe97efac73e1a2854ec6b6554bf0bb7021
Anton Sotirov <anton.k.sotirov@gmail.com>
2018-12-05 14:09:11 +0200

    0.1.3 new async cache

--- cache.js
+++ cache.js
@@ -3,11 +3,27 @@ const cacheManager = require("cache-manager"),
     redisStore = require('cache-manager-redis');
 
 module.exports = function (options, imports, register) {
+    let prefix = options.prefix || 'cache';
+
+    function prefixFirst(fn) {
+        return function (key, ...rest) {
+            return fn(`${prefix}:${key}`, ...rest);
+        }
+    }
+
     try {
         let redisOptions = options.redis || (imports['options.cache'] ? imports['options.cache'].redis : null);
-        if (!redisOptions) {
+        if (redisOptions === undefined) {
             throw new Error(`[BuilderCache] options.redis is not provided. Make sure to include {cache:redis} in options plugin or include {redis} in serverConfig for legacy builder`)
         }
+        if (!redisOptions) {
+            console.log('Cache not initialized');
+            return {
+                cache: {}
+            }
+        }
+
+        console.log('[REDIS.OPTIONS]', redisOptions);
         const cache = cacheManager.caching({
             ignoreCacheErrors: true,
             store: redisStore,
@@ -19,33 +35,44 @@ module.exports = function (options, imports, register) {
             },
             retry_strategy: function (options) {
                 if (options.attempt > 1) {
-                    // Stop retrying after first attempts.
+                    // Stop retrying after second attempt.
+                    console.error(`[REDIS.ERROR]`, options.error);
                     return undefined;
                 }
-                // Increase reconnect delay by 150ms.
-                return options.attempt * 150;
+                // Increase reconnect delay by 1 sec.
+                return options.attempt * 1;
             }
         });
 
-        (function () {
-            let prefix = 'cache';
+        let wrapPrefixed = prefixFirst(cache.wrap);
 
-            function prefixFirst(fn) {
-                return function (key, ...rest) {
-                    fn(`${prefix}:${key}`, ...rest);
-                }
-            }
-
-            register(null, {
-                cache: Object.assign(Object.create(Object.getPrototypeOf(cache)), cache, {
-                    wrap: prefixFirst(cache.wrap),
-                    get: prefixFirst(cache.get),
-                    set: prefixFirst(cache.set),
-                    del: prefixFirst(cache.del),
-                    ttl: prefixFirst(cache.ttl)
-                })
-            });
-        }())
+        register(null, {
+            cache: Object.assign(Object.create(Object.getPrototypeOf(cache)), cache, {
+                wrap: (key, handler, cb) => {
+                    try {
+                        wrapPrefixed(key, async (cb) => {
+                            try {
+                                cb(null, await handler());
+                            } catch (err) {
+                                cb(err);
+                            }
+                        }, cb);
+                    } catch (err) {
+                        (async () => {
+                            try {
+                                cb(null, await handler());
+                            } catch (err) {
+                                cb(err);
+                            }
+                        })();
+                    }
+                },
+                get: prefixFirst(cache.get),
+                set: prefixFirst(cache.set),
+                del: prefixFirst(cache.del),
+                ttl: prefixFirst(cache.ttl)
+            })
+        });
     } catch (err) {
         register(err);
     }
